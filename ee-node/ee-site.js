1e58d77654f842575896df601b9038fc63de0372
harshadyeola <harshad.yeola@rtcamp.com>
2014-11-13 15:48:27 +0530

    minor changes

--- ee-node/ee-site.js
+++ ee-node/ee-site.js
@@ -5,20 +5,3 @@ var program = require('commander');
 program
   .command('create <site>', 'create site site')
   .parse(process.argv); 
-#!/usr/bin/env node
-
-var program = require('commander');
-
-program
-  .option('--nginx', 'install nginx package')
-  .option('--php', 'install php package')
-  .option('--mysql', 'install mysql package')
-  .parse(process.argv);
-
-console.log();
-
-if (program.nginx) console.log('  ee stack : install nginx');
-if (program.php) console.log('  ee stack : install php');
-if (program.mysql) console.log('  ee stack: install mysql');
-
-console.log();
